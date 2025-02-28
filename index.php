111ef5d425e269ea0f101fd74f1218281de58c16
Ty Steinbach <152814492+Aphotic04@users.noreply.github.com>
2025-02-27 22:08:36 -0600

    Update index.php
    Remove directed database values

--- index.php
+++ index.php
@@ -22,10 +22,10 @@ $_SESSION['reset'] = 0;
 
 // Database connection details
 
-$host = 'localhost';
-$user = 'root';
-$pass = 'mysql'; 
-$dbname = 'csc450temp';
+$host = '';
+$user = '';
+$pass = ''; 
+$dbname = '';
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 
