094c85dd7397c257f6d2b7b8dbb6276806c6be4d
Ty Steinbach <152814492+Aphotic04@users.noreply.github.com>
2025-03-17 21:12:54 -0500

    Create footer.php

---
+++ EventCalendarPHP-main/templates/footer.php
@@ -0,0 +1,10 @@
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.27.0/moment.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.2/fullcalendar.min.js'></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js"></script>
+<script src="./assets/js/script.js"></script>
+<script src="./assets/js/friendScript.js"></script>
+</main>
+</body>
+</html>
