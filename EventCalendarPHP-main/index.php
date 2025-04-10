319cbd63b7aac878235327b3da6cb45a4c18253a
Ty Steinbach <152814492+Aphotic04@users.noreply.github.com>
2025-04-09 20:32:43 -0500

    Update index.php

--- EventCalendarPHP-main/index.php
+++ EventCalendarPHP-main/index.php
@@ -18,61 +18,8 @@ https://github.com/obadaKraishan/EventCalendarPHP
 -->
 <?php
     session_start();
-    // Confirm login of user from index
-    if (!isset($_SESSION['username'])) {
-        header("Location: ../index.php");
-        exit();
-    }
-
-    $host = 'localhost';
-    $user = 'root';
-    $pass = 'mysql'; 
-    $dbname = 'csc450temp';
-
-    $conn = new mysqli($host, $user, $pass, $dbname);
-
-
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-
-    // If host, db, user, or pass is incorrect create error
-    if ($conn->connect_error) {
-        die("Failed to connect: " . $conn->connect_error);
-    }
-
-    $sql = "SELECT * FROM users WHERE username = ? LIMIT 1";
-        
-    // Set up a prepared statement
-    if($stmt = $conn->prepare($sql)) {
-
-        // Pass the parameters
-        $stmt->bind_param("s", $_SESSION['username']);
-
-        if($stmt->errno) {
-            print_r("stmt prepare( ) had error."); 
-        }
-
-        // Execute the query
-        $stmt->execute();
-        if($stmt->errno) {
-            print_r("Could not execute prepared statement");
-        }
-
-        // Fetch the results
-        $result = $stmt->get_result();
-
-        // Free results
-        $stmt->free_result( );
-        
-        
-        // Close the statement
-        $stmt->close( );
-    } // end of if($conn->prepare($sql))
-
-    $row = $result->fetch_assoc();
-
-    $_SESSION['user_id'] = $row["id"];
+    
+    include 'set_user_id.php';
 
     include 'templates/header.php';
     include 'templates/navbar.php';
